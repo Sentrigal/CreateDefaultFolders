783271cf1f27e20e014ad649e1f7e1c55016d585
Sentrigal <sentrigal@gmail.com>
2023-02-22 20:02:51 +0100

    Initial

---
+++ CreateFolderStructure.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateFolderStructure : EditorWindow
+{ 
+    private static string rootFolderName = "RootFolder";
+    private static bool createGitPlaceholder = true;
+
+    [MenuItem("Assets/Create Default Folders")]
+    private static void CreateWindow()
+    {
+        CreateFolderStructure window = ScriptableObject.CreateInstance<CreateFolderStructure>();
+        window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 300);
+        window.Show();
+    }
+
+    private static void CreateFolders ()
+    {
+        /// ---- How To Use ---- \\\
+        /// First, create your main folder like so:
+        /// VirtualFolder rootFolder = new VirtualFolder("Assets", rootFolderName);
+        /// This will create a folder with whatever name you entered in the window, which you can find in Unity under Assets -> Create Default Folders.
+        /// In order to add sub folders to this, you simply call either AddSubFolder or AddSubFolders on it, like so:
+        /// rootFolder.AddSubFolder("YourNewSubFolder");
+        /// This will add a new folder into the root folder, which ultimately looks like this: Assets/RootFolder/YourNewSubFolder.
+        /// You can call AddSubFolder again to add yet another sub folder to THAT sub folder, like this:
+        /// rootFolder.AddSubFolder("YourNewSubFolder").AddSubFolder("YetAnotherFolder");
+        /// Which will result in this structure: Assets/RootFolder/YourNewSubFolder/YetAnotherFolder.
+        /// You can chain these as much as you'd like.
+        /// Furthermore, you can add multiple folders at once by calling AddSubFolders, like this:
+        /// rootFolder.AddSubFolders(new string[] {"FolderOne", "FolderTwo", "FolderThree"});
+        /// Note that this does NOT return the folders, so you can't chain on these. 
+        /// So you should probably only do this on the deepest part of your hierachy.
+        /// Finally, below is an example implementation that will probably suit most people just fine.
+        /// But feel free to change it to your liking.
+
+
+        // Create the root folder which you can name
+        VirtualFolder rootFolder = new VirtualFolder("Assets", rootFolderName);
+
+        // Create a "Resources" folder by adding it to the root folder.
+        VirtualFolder resourceFolder = rootFolder.AddSubFolder("Resources");
+        // Add a bunch of folders into this "Resources" folder.
+        resourceFolder.AddSubFolder("Materials");
+        resourceFolder.AddSubFolder("Meshes").AddSubFolder("Textures");
+        resourceFolder.AddSubFolder("Sprites");
+        resourceFolder.AddSubFolder("Textures");
+        resourceFolder.AddSubFolder("Animations");
+        resourceFolder.AddSubFolder("Shaders");
+        resourceFolder.AddSubFolder("Audio").AddSubFolders(new string[] {"SFX", "Music" });
+        // Add more top level folders.
+        rootFolder.AddSubFolders(new string[] { "Scripts", "Prefabs", "Scenes", "Data"});
+        // By calling Realize() on the root folder, it will automatically create every folder we've added to it or its sub folders!
+        rootFolder.Realize(createGitPlaceholder);
+
+        AssetDatabase.Refresh();
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Enter name of the root folder: ");
+        rootFolderName = EditorGUILayout.TextField("Root:", rootFolderName);
+        createGitPlaceholder = EditorGUILayout.ToggleLeft("Placeholders", createGitPlaceholder);
+        EditorGUILayout.HelpBox("When ticked, dummy text files will be created in every folder in order to give Git (and similar tools) something to upload. Without this, Git will ignore these folders until they contain some other files.", MessageType.Info);
+
+
+        if (GUILayout.Button("Generate"))
+        {
+            CreateFolders();
+            this.Close();
+        }
+        EditorGUILayout.EndVertical();
+    }
+}
---
+++ VirtualFolder.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public struct VirtualFolder
+{
+    public string Root { get => root; private set => root = value; }
+    public string Name { get => name; private set => name = value; }
+    public List<VirtualFolder> SubFolders { get => subFolders; private set => subFolders = value; }
+
+    string root;
+    string name;
+    List<VirtualFolder> subFolders;
+
+    public VirtualFolder (string root, string name)
+    {
+        this.root = root;
+        this.name = name;
+        this.subFolders = new List<VirtualFolder>();
+    }
+    /// <summary>
+    /// Adds a new sub folder to this folder and returns it.
+    /// </summary>
+    /// <param name="name">Name of the new subfolder.</param>
+    /// <returns>The new virtual folder.</returns>
+    public VirtualFolder AddSubFolder (string name)
+    {
+        var subFolder = new VirtualFolder(root + "/" + this.name + "/", name);
+        subFolders.Add(subFolder);
+        return subFolder;
+    }
+
+    /// <summary>
+    /// Adds multiple new sub folders to this folder.
+    /// </summary>
+    /// <param name="names">An array of names for the sub folders.</param>
+    public void AddSubFolders (string[] names)
+    {
+        foreach (var item in names)
+        {
+            AddSubFolder(item);
+        }
+    }
+    /// <summary>
+    /// Turns this folder (and all subfolders (and their subfolders)) into actual directories, provided they don't already exist.
+    /// </summary>
+    /// <param name="createGitDummyFile">Whether or not a dummy text file is created for Git, because Git ignores empty folders.</param>
+    public void Realize(bool createGitDummyFile = false)
+    {
+        string fullPath = root + "/" + name;
+
+        if (!Directory.Exists(fullPath))
+        {
+            CreateFolder(fullPath, createGitDummyFile);
+        }
+        if (subFolders != null && subFolders.Count > 0)
+        {
+            foreach (var folder in SubFolders)
+            {
+                folder.Realize(createGitDummyFile);
+            }
+        }
+    }
+
+    void CreateFolder (string fullPath, bool createGitDummyFile = false)
+    {
+        Directory.CreateDirectory(fullPath);
+        if (createGitDummyFile)
+        {
+            using (var fs = File.CreateText(fullPath + "/Dummy.txt"))
+            {
+                fs.WriteLine("Dummy file for Git for the path: " + fullPath);
+            }
+        }
+    }
+}
